c0eb0a719b3ddc5bdfccb4b1d9509f5b4aa1447a
allenyang <aryang@caltech.edu>
2022-08-25 10:55:29 -0700

    consolidated plotter
    issue: #2

--- smartfin/sfPlotter.py
+++ smartfin/sfPlotter.py
@@ -9,6 +9,8 @@ import os
 import glob
 import argparse
 
+DEGREE_CHANGE_C_CONSIDERED_SETTLED = 0.2
+
 def plotFile(fileName:str, output_dir:str)->str:
     df = pd.read_csv(fileName)
     
@@ -127,8 +129,131 @@ def plotFile(fileName:str, output_dir:str)->str:
         plt.grid()
         plt.savefig(os.path.join(output_dir, 'Battery.png'))
         plt.close()
+    
+    if "Z Magnetic Field" in df.columns and "X Magnetic Field" in df.columns:
+        plotHeadings(getHeading(df), df["timestamp"], output_dir)
+        
+    plotConsolidated(df, output_dir)
+
+def plotConsolidated(df, output_dir):
+    fig, axs = plt.subplots(3,4,figsize=(25,28))
+
+    axs[0][0].plot(df['timestamp'], df['X Acceleration'])
+    axs[0][0].set_title(output_dir + '\n\nX acc vs timestamp')
+    axs[0][0].set_ylim(-3,3)
+    axs[0][0].set_xlabel("time (s)")
+    axs[0][0].set_ylabel("acceleration (g)")
+    axs[0][0].axhline(0, color="orange", linestyle="dotted")
+    axs[0][0].axhline(1, color="orange", linestyle="dotted", label = '1g')
+    axs[0][1].plot(df['timestamp'], df['Y Acceleration'])
+    axs[0][1].set_ylim(-3,3)
+    axs[0][1].set_title("Y acc vs timestamp")
+    axs[0][1].axhline(0, color="orange", linestyle="dotted")
+    axs[0][1].axhline(1, color="orange", linestyle="dotted")
+    axs[0][1].set_xlabel("time (s)")
+    axs[0][1].set_ylabel("acceleration (g)")
+    axs[0][2].plot(df['timestamp'], df['Z Acceleration'])
+    axs[0][2].set_ylim(-3,3)
+    axs[0][2].set_title("Z acc vs timestamp")
+    axs[0][2].axhline(0, color="orange", linestyle="dotted")
+    axs[0][2].axhline(1, color="orange", linestyle="dotted")
+    axs[0][2].set_xlabel("time (s)")
+    axs[0][2].set_ylabel("acceleration (g)")
+
+    axs[0][3].plot(df['timestamp'], df['Temperature'])
+    axs[0][3].axhline(df['Temperature'].median(), color="orange", linestyle="dotted")
+    axs[0][3].set_title("temperature vs timestamp\nMedian: " + str(round(df['Temperature'].median(),2)) + " degrees C")
+    axs[0][3].set_xlabel("time (s)")
+    axs[0][3].set_ylabel("temperature (C)")
 
 
+    axs[1][0].plot(df['timestamp'], df['X Angular Velocity'])
+    axs[1][0].set_title("X gyro vs timestamp")
+    axs[1][0].set_ylim(-300,300) 
+    axs[1][0].axhline(0, color="orange", linestyle="dotted")
+    axs[1][0].set_xlabel("time (s)")
+    axs[1][0].set_ylabel("angular velocity (rad/sec)")
+    axs[1][1].plot(df['timestamp'], df['Y Angular Velocity'])
+    axs[1][1].set_ylim(-300,300)
+    axs[1][1].axhline(0, color="orange", linestyle="dotted")
+    axs[1][1].set_title("Y gyro vs timestamp")
+    axs[1][1].set_xlabel("time (s)")
+    axs[1][1].set_ylabel("angular velocity (rad/sec)")
+    axs[1][2].plot(df['timestamp'], df['Z Angular Velocity'])
+    axs[1][2].set_ylim(-300,300)
+    axs[1][2].set_title("Z gyro vs timestamp")
+    axs[1][2].axhline(0, color="orange", linestyle="dotted")
+    axs[1][2].set_xlabel("time (s)")
+    axs[1][2].set_ylabel("angular velocity (rad/sec)")
+    axs[1][3].plot(df['timestamp'], df['Temperature'].diff(periods=1))
+    axs[1][3].plot(df['timestamp'], df['Temperature'].diff(periods=300))
+    axs[1][3].plot(df['timestamp'], df['Temperature'].diff(periods=-300))
+    axs[1][3].set_title("Δ temperature over\nBlue: 1s, Orange: 5min, Green: -5min")
+    axs[1][3].axhline(0, color="orange", linestyle="dotted")
+    axs[1][3].axhline(DEGREE_CHANGE_C_CONSIDERED_SETTLED, color="orange", linestyle="dotted")
+    axs[1][3].axhline(-DEGREE_CHANGE_C_CONSIDERED_SETTLED, color="orange", linestyle="dotted")
+    axs[1][3].set_ylim(-1,1)
+    axs[1][3].set_xlabel("time (s)")
+    axs[1][3].set_ylabel("Δ temperature (C)")
+
+    axs[2][0].scatter( df['timestamp'], df['X Magnetic Field'])
+    axs[2][0].set_ylim(-6000,6000)
+    axs[2][0].set_title("X mag vs timestamp")
+    axs[2][0].set_xlabel("time (s)")
+    axs[2][0].set_ylabel("magnetic field strength (nT)")
+    axs[2][1].scatter(df['timestamp'], df['Y Magnetic Field'])
+    axs[2][1].set_ylim(-6000,6000)
+    axs[2][1].set_title("Y mag vs timestamp")
+    axs[2][1].set_xlabel("time (s)")
+    axs[2][1].set_ylabel("magnetic field strength (nT)")
+    axs[2][2].scatter(df['timestamp'], df['Z Magnetic Field'])
+    axs[2][2].set_ylim(-6000,6000)
+    axs[2][2].set_title("Z mag vs timestamp")
+    axs[2][2].set_xlabel("time (s)")
+    axs[2][2].set_ylabel("magnetic field strength (nT)")
+
+    df['Water Detect'] = df['Water Detect'].replace({np.nan:0.1})
+    axs[2][3].scatter(df['timestamp'], df['Water Detect'] > 0)
+    axs[2][3].set_title("timestamp vs water detect")
+    axs[2][3].set_xlabel("time (s)")
+    axs[2][3].set_ylabel("water detected (1/0)")
+
+    plt.subplots_adjust(left=0.1,
+                bottom=0.1, 
+                right=0.9, 
+                top=0.9, 
+                wspace=0.4, 
+                hspace=0.4)
+
+    plt.savefig(os.path.join(output_dir, "ConsolidatedGraphs.png"))
+
+def getHeading(df_data):
+    z_arr = df_data.loc[:,"Z Magnetic Field"].to_numpy()
+    x_arr = df_data.loc[:,"X Magnetic Field"].to_numpy()
+    
+    return np.arctan2(z_arr, x_arr) * 180 / np.pi
+
+def plotHeadings(heading_arr, timestamps, output_dir):
+    plt.scatter(timestamps, heading_arr, s=10) #Cartesian Graph
+    plt.title('Heading vs Time')
+    plt.xlabel('Time')
+    plt.ylabel('Degrees')
+    plt.savefig(os.path.join(output_dir, "CartesianHeadings.png"), dpi=300)
+
+    plt.clf()
+
+    r = timestamps #Polar graph
+    theta = (heading_arr / 180) * np.pi + np.pi / 2
+    fig, ax = plt.subplots(subplot_kw={'projection': 'polar'})
+    labels = [item.get_text() for item in ax.get_xticklabels()]
+    labels[0], labels[1], labels[2], labels[3], \
+    labels[4], labels[5], labels[6], labels[7] = 'E', 'NE', 'N', 'NW', 'W', 'SW', 'S', 'SE'
+    ax.set_xticklabels(labels)
+    ax.set_yticklabels([])
+    ax.scatter(theta, r, marker=',', s=1, cmap = 'winter')
+    plt.savefig(os.path.join(output_dir, "PolarHeadings.png"), dpi=1000)
+        
+        
 def main():
     parser = argparse.ArgumentParser("Smartfin Data Plotter")
     parser.add_argument('csv_fp', default=None, nargs='?')
